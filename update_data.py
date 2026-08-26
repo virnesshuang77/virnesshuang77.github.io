96659d37b3002241355bf292ead035118378bbe0
virnesshuang77 <yy0984216000@gmail.com>
2026-08-26 18:34:46 +0800

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -95,37 +95,29 @@ def create_money_rank(
 
     def make_rank(column):
 
-        # ----------------------------------------------------
-        # 買超 TOP 100
-        # 正數由大到小
-        # ----------------------------------------------------
-
-        buy_result = (
-            df[
-                [
-                    "stock_id",
-                    "stock_name",
-                    "close_price",
-                    column
-                ]
+    result = (
+        df[
+            [
+                "stock_id",
+                "stock_name",
+                "close_price",
+                column
             ]
-            .loc[
-                lambda x:
-                    x[column] > 0
-            ]
-            .sort_values(
-                column,
-                ascending=False
-            )
-            .reset_index(
-                drop=True
-            )
-            .head(100)
+        ]
+        .sort_values(
+            column,
+            ascending=False
         )
+        .reset_index(
+            drop=True
+        )
+    )
 
-        buy_result["rank"] = (
-            buy_result.index + 1
-        )
+    result["rank"] = (
+        result.index + 1
+    )
+
+    return result.head(100)
 
         # ----------------------------------------------------
         # 賣超 TOP 100
